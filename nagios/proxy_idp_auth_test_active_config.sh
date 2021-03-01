eaf9552bad47f24081b3ee5878f4d9abe8b91903
Pavel Vyskocil <Pavel.Vyskocil@cesnet.cz>
2021-03-01 14:18:42 +0100

    Changes in proxy_idp_auth_test_active_saml/oidc.sh
    * Use configuration file instead of command line args
    * Only one argument is passed - the path to the configuration file

---
+++ nagios/proxy_idp_auth_test_active_config.sh
@@ -0,0 +1,31 @@
+# The urls of tested SP
+# For example: https://aai-playground.ics.muni.cz/simplesaml/nagios_check.php?proxy_idp=cesnet&authentication=muni
+AAI_SAML_TEST_SITE=""
+AAI_OIDC_TEST_SITE=""
+MUNI_SAML_TEST_SITE=""
+MUNI_OIDC_TEST_SITE=""
+CESNET_SAML_TEST_SITE=""
+CESNET_OIDC_TEST_SITE=""
+
+# The url of logins form of used IdP
+# For example: https://idp2.ics.muni.cz/idp/Authn/UserPassword
+AAI_LOGIN_SITE=""
+MUNI_LOGIN_SITE=""
+CESNET_LOGIN_SITE=""
+
+# Fill in logins
+AAI_LOGIN=""
+MUNI_LOGIN=""
+CESNET_LOGIN=""
+
+# Fill in passwords as string
+AAI_PASSWORD=""
+MUNI_PASSWORD=""
+CESNET_PASSWORD=""
+
+
+# How long is normal for total roundtrip (seconds)
+WARNING_TIME=10
+
+# Timeout time
+TIMEOUT_TIME=40
